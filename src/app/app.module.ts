4c65bc20768c56eafa002b9318512274f071d1ed
DGL-64357-1\BNI <Andrian.Kusuma.Solihin@bni.co.id>
2023-12-05 15:18:34 +0700

    feat: home layouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,31 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ToggleTabsComponent } from './components/toggle-tabs/toggle-tabs.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     TopbarComponent,
-    HomeComponent
+    HomeComponent,
+    ToggleTabsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  
 })
 export class HomeComponent {
-
+  fontStyleControl = new FormControl('');
+  fontStyle?: string;
 }
