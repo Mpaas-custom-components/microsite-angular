31f91c2a747d8edcbb903d9423da0384049d8b04
Juan Alfonsus <Juan.Alfonsus@bni.co.id>
2023-12-06 11:29:16 +0700

    feat: breakdown category page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BreakdownCategoryComponent } from './pages/breakdown-category/breakdown-category.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { TransactionCardComponent } from './components/transaction-card/transaction-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     TopbarComponent,
-    HomeComponent
+    HomeComponent,
+    BreakdownCategoryComponent,
+    SliderComponent,
+    TransactionCardComponent
   ],
   imports: [
     BrowserModule,
